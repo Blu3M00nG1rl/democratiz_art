2ecfffb7a2438f5f41a62d5b594fb7aaa8241481
Sophie Constantin <scsophieconstantin@gmail.com>
2022-12-10 22:07:20 +0100

    deploiement goerli nouveau contrat

--- src/context/constants.js
+++ src/context/constants.js
@@ -2,11 +2,11 @@ import Democratiz_Art from "../artifacts/contracts/Democratiz_Art.sol/Democratiz
 import { ethers } from "ethers";
 //Localhost : 0x1A8141567Eed7F996FE6A09AE3C639e4E952056E
 //Mumbai : 0x8F6fa783386d0b447a2CbCde213102Da4662dD76
-//Goerli : 0x54C8FF4Ec74eEd4baB9B933F76099C210EB698a5
+//Goerli : 0x9C0edD951412d700b741BfeDE29916C2D93bD3A4
 
 //Contract Address and ABI
 export const Democratiz_ArtAddress =
-    "0x54C8FF4Ec74eEd4baB9B933F76099C210EB698a5";
+    "0x9C0edD951412d700b741BfeDE29916C2D93bD3A4";
 export const Democratiz_ArtABI = Democratiz_Art.abi;
 
 //Fetching Smart Contract
